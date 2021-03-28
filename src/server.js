314ce00db44add1af7ce087033a6d723fee40ea1
Daniel Rosel <danalves24@outlook.com>
2021-03-28 19:01:49 +0200

    init

---
+++ src/server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors');
+app.use(bodyParser.json()) // to process params
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Saniroot",
+  database: "edi",
+  port: 3306
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to database!");
+});
+//
+
+
+
+
+app.use(cors())
+
+
+
+var controler_get = {
+
+	"filter" : function(req, res) {
+		var param = req.body
+		var conditions = []
+		for(var a in param) {
+			console.log(a)
+			var condition = `${a} like '%${param[a]}%'`
+			conditions.push(condition)
+		}
+		var conditions = conditions.join(" and ")
+		console.log(conditions)
+		var sql = `select * from colleges where ${conditions}`
+		con.query(sql, function(err, result){
+			if (err) throw err;
+			result = result
+			var out = {"found": result.length,"results": result}
+			res.json(out)
+		})
+	},
+	"about": function(req, res) {
+		var param = req.params.param;		
+		var sql = "select * from colleges where WEBSITE like '%"+param+"%'";
+		console.log(sql)
+		con.query(sql, function(err, result) {
+			res.send(result)
+		})
+	},	
+	"get_all": function(req, res){
+		var param = req.params.param==undefined?10:req.params.param;
+		var sql = "select * from colleges;";
+		con.query(sql, (err, result) => {
+			if(err) throw err;
+			res.send(result)
+		})
+//		res.send("sending all "+param)
+	}
+
+}
+
+
+app.get("/edi/:command/:param",(req, res)=>{
+	controler_get[req.params.command](req, res);
+})
+
+
+app.get("/edi/:command",(req, res)=>{
+	controler_get[req.params.command](req, res);
+})
+
+
+app.listen(8000, () => console.log("server is listening on port 8000, happy coding"))
